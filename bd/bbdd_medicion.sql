aac43c8132f1289304a615be46e0b9e9b9088792
ruben021000 <55706771+ruben021000@users.noreply.github.com>
2022-10-18 02:19:31 +0200

    Base de datos

---
+++ bd/bbdd_medicion.sql
@@ -0,0 +1,5 @@
+create table Mediciones (
+id char(9) not null,
+medicion varchar(15) not null,
+primary key (id)
+);
---
+++ bd/insertar_en_bbdd.sql
@@ -0,0 +1,2 @@
+insert into Mediciones values ('1','1234');
+insert into Mediciones values ('2','4321');
